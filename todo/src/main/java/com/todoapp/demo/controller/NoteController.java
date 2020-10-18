a212844d54f61b5fa9b68e0b4f9b8598259c3db3
deimos0992 <49685971+deimos0992@users.noreply.github.com>
2020-10-18 22:13:25 +0200

    metodo save

--- todo/src/main/java/com/todoapp/demo/controller/NoteController.java
+++ todo/src/main/java/com/todoapp/demo/controller/NoteController.java
@@ -2,11 +2,10 @@ package com.todoapp.demo.controller;
 
 import com.todoapp.demo.model.Note;
 import com.todoapp.demo.repository.NoteRepository;
+import com.todoapp.demo.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,9 +16,22 @@ public class NoteController {
     @Autowired
     private NoteRepository noteRepository;
 
+    @Autowired
+    private NoteService noteservice;
+
+
     @GetMapping("/all")
     public ResponseEntity<List<Note>> getAllNotes(){
 
         return ResponseEntity.ok(noteRepository.findAll());
+    }
+
+    // Save Single Note
+    @PostMapping
+    public ResponseEntity<Note> postSingleNote(@RequestBody Note noteDto){
+
+        Note note = new Note();
+        note = noteservice.saveSingleNote(noteDto);
+        return ResponseEntity.ok(note);
     }
 }
--- todo/src/main/java/com/todoapp/demo/service/NoteService.java
+++ todo/src/main/java/com/todoapp/demo/service/NoteService.java
@@ -1,8 +1,14 @@
 package com.todoapp.demo.service;
 
+import com.todoapp.demo.model.Note;
 import com.todoapp.demo.repository.NoteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Service
 @AllArgsConstructor
@@ -10,6 +16,15 @@ public class NoteService {
 
     private final NoteRepository noteRepository;
 
+    @Transactional
+    public Note saveSingleNote(Note payload){
+        Note newNote = new Note();
+        newNote.setTitolo(payload.getTitolo());
+        newNote.setDescrizione(payload.getDescrizione());
+        newNote.setCreatedAt(Date.from(Instant.now()));
+        newNote.setUpdateAt(Date.from(Instant.now()));
+        return noteRepository.save(newNote);
+    }
 
 
 }
